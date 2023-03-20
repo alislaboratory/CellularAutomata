3772e225a6a8c1dbe8e17cc967b7372ce9725db6
ashrafyind <alia@DESKTOP-HRMP1U5>
2023-03-20 17:38:45 +1100

    first life test

--- GameOfLifePython/main.py
+++ GameOfLifePython/main.py
@@ -1,3 +1,35 @@
 from life import Life, Vector
+import time
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 game = Life(Vector(10,10))
+
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 16
+options.parallel = 1
+options.hardware_mapping = 'regular'
+
+matrix = RGBMatrix(options=options)
+
+canvas = matrix.CreateFrameCanvas()
+
+
+while True:
+    time.sleep(0.1)
+    
+    grid = game.get_grid()
+
+    grid[6][3].state = True
+
+    game.set_grid(grid)
+
+    for i in len(grid):
+        for j in len(i):
+            canvas.SetPixel(i, j, 255, 255, 255)
+
+    canvas = matrix.SwapOnVSync(canvas)
+
+
+
